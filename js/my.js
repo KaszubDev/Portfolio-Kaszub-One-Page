e1919b77453d0215674aedfa8a16e23a88b1781d
Krzysztof <kaszubowskikrzys@gmail.com>
2018-10-25 14:47:46 +0200

    preloader fix

--- js/my.js
+++ js/my.js
@@ -88,11 +88,30 @@ function navToggle(x) {
 };
 
 /* PRELOAD FADEOUT FUNCTION */
+/*
 if (document.readyState === "complete") {
 
 $('#preloader').fadeOut('slow',function(){$(this).remove();});
 
 };
+*/
+function onReady(callback) {
+  var intervalId = window.setInterval(function() {
+    if (document.getElementsByTagName('body')[0] !== undefined) {
+      window.clearInterval(intervalId);
+      callback.call(this);
+    }
+  }, 1000);
+}
+
+function setVisible(selector, visible) {
+  document.querySelector(selector).style.display = visible ? 'block' : 'none';
+}
+
+onReady(function() {
+  setVisible('.page', true);
+  setVisible('#preloader', false);
+});
 
 $(window).ready(function(){
 AOS.init({
